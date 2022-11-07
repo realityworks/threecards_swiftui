29515e32fd871f73f7a028e40dd9a1997f9de567
Piotr Suwara <psuwara@realityworks.com.au>
2022-11-07 15:57:35 +1100

    Initial version

---
+++ threecards/threecardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  threecardsApp.swift
+//  threecards
+//
+//  Created by Piotr Suwara on 7/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct threecardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
