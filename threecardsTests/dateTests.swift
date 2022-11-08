55f68549a74eaef695913ce0874dbeeafc095a9f
Piotr Suwara <psuwara@realityworks.com.au>
2022-11-09 09:55:17 +1100

    Minor cleanup

--- threecardsTests/dateTests.swift
+++ threecardsTests/dateTests.swift
@@ -18,11 +18,6 @@ final class dateTests: XCTestCase {
     }
 
     func testExpiry() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
         // Ten days in the future
         let dateNotExpired = Date(fromNowDays: 5)
         XCTAssertTrue(dateNotExpired.expired == false)
